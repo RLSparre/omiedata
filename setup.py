069023c0ab6e6dfaa9d4467223f965d6345c1e69
RLSparre <rasmuslodbergsparre@gmail.com>
2024-01-23 20:28:09 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=readme,
     author='Rasmus Lodberg Sparre',
     author_email='rasmuslodbergsparre@gmail.com',
-    url='https://https://github.com/RLSparre/omiedata',
+    url='https://github.com/RLSparre/omiedata',
     license=license,
     packages=find_packages(exclude=('tests')),
     include_package_data=True
